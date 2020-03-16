d03025160ade93d39c0d94fb0b3630613a5a38c3
Lloyd van Zaalen <lfb0801@github.com>
2020-03-16 13:55:12 +0100

    implement lombok onto Skittle class

--- src/main/java/com/vanhalen/domain/Skittle.java
+++ src/main/java/com/vanhalen/domain/Skittle.java
@@ -1,21 +1,16 @@
 package com.vanhalen.domain;
 
+import lombok.Getter;
+
 import javax.persistence.Entity;
 import java.awt.*;
 
 @Entity
+@Getter
 public class Skittle {
     private Color color;
 
     public Skittle(Color color) {
-        this.color = color;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-
-    public void setColor(Color color) {
         this.color = color;
     }
 }
