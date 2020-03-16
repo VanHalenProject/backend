c153357a77c5978b8858dd7a963f148bb5cfa315
Lloyd van Zaalen <lfb0801@github.com>
2020-03-16 13:49:40 +0100

    added missing annotations

--- src/main/java/com/vanhalen/logic/SkittleLogic.java
+++ src/main/java/com/vanhalen/logic/SkittleLogic.java
@@ -3,7 +3,9 @@ package com.vanhalen.logic;
 import com.vanhalen.interfaces.SkittleLogicInterface;
 import com.vanhalen.repositories.SkittleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SkittleLogic implements SkittleLogicInterface {
     @Autowired
     private SkittleRepository repository;
--- src/main/java/com/vanhalen/repositories/SkittleRepository.java
+++ src/main/java/com/vanhalen/repositories/SkittleRepository.java
@@ -2,6 +2,8 @@ package com.vanhalen.repositories;
 
 import com.vanhalen.domain.Skittle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SkittleRepository extends JpaRepository<Skittle, Integer> {
 }
